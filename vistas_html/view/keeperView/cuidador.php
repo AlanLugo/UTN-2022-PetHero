b0e51dcc3ef307a1ec1dfaae13a4354098033efc
nilo000 <nico2000@live.com.ar>
2022-10-21 00:02:03 -0300

    primeras vistas

---
+++ vistas_html/view/keeperView/cuidador.php
@@ -0,0 +1,6 @@
+<?php include("../includes/head.php") ?>
+
+<h1>cuidador</h1>
+
+
+<?php include("../includes/footer.php") ?>
---
+++ vistas_html/view/usuarioView/reserva.php
@@ -0,0 +1,10 @@
+<?php include("../includes/head.php") ?>
+<?php include("../includes/nav.php") ?>
+
+<div class="pagReserva">
+<div class="container">
+<h1 >ver reservas de la bs y confirmar un pago, cambiar si estado de false a true </h1>
+
+</div>
+</div>
+<?php include("../includes/footer.php") ?>
